a590324e12f813d2276df78c4b4a47374451e107
Abed <abed.hassani.idrissi@gmail.com>
2023-05-22 18:15:40 +0100

    mhassani

--- minishell.c
+++ minishell.c
@@ -6,37 +6,35 @@
 /*   By: mhassani <mhassani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 21:53:19 by mhassani          #+#    #+#             */
-/*   Updated: 2023/05/22 16:54:16 by mhassani         ###   ########.fr       */
+/*   Updated: 2023/05/22 18:14:06 by mhassani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	syntax_errors(int len, char *cmd)
+void	syntax_errors(char *cmd, int i)
 {
-	int	i;
+	int	error;
 
+	error = 0;
 	i = 0;
-	len = ft_strlen(cmd);
-	if (cmd[len - 1] == '>' || cmd[len - 1] == '<')
-		write(2, "minishell: syntax error\n", 24);
-	while (cmd[i] == ' '|| cmd[i] == '\t')
+	while (cmd[i] == ' ' || cmd[i] == '\t')
 		i++;
+	if (cmd[i] == '|')
+	{
+		write(2, "minishell: syntax error\n", 24);
+		error++;
+	}
 	while (cmd[i])
 	{
 		if (cmd[i] == '|')
 		{
-			if(cmd[0] == '|')
-			{
-				write(2, "minishell: syntax error\n", 24);
-				break;
-			}
-			while(cmd[i + 1] == ' ' || cmd[i + 1] == '\t')
+			while (cmd[i + 1] == ' ' || cmd[i + 1] == '\t')
 				i++;
-			if(cmd[i + 1] == '\0' || cmd[i + 1] == '|')
+			if ((cmd[i + 1] == '\0' || cmd[i + 1] == '|') && !error)
 			{
 				write(2, "minishell: syntax error\n", 24);
-				break;
+				break ;
 			}
 		}
 		i++;
@@ -62,7 +60,7 @@ int	main(void)
 		}
 		add_history(cmd);
 		//printf("the cmd is: %s\n", cmd);
-		syntax_errors(len, cmd);
+		syntax_errors(cmd, i);
 		token = ft_split(cmd, '|');
 		i = 0;
 		while (token[i])
--- minishell.h
+++ minishell.h
@@ -6,13 +6,19 @@
 /*   By: mhassani <mhassani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 21:52:55 by mhassani          #+#    #+#             */
-/*   Updated: 2023/05/21 16:43:08 by mhassani         ###   ########.fr       */
+/*   Updated: 2023/05/22 18:14:17 by mhassani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
 #include <string.h>
+
+typedef struct s_data
+{
+	int	i;
+	int	error;
+} t_data;
